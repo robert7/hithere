232ea5ab7cffcfea2858841f24aebaec264cd11a
Robert Spiegel <r.spiegel@netconomy.net>
2018-09-12 11:08:09 +0200

    Updated code

--- hithere.c
+++ hithere.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
     if (argc == 1)
-        printf("hi there, world\n!");
+        printf("hi there, WORLD\n!");
     else {
         printf("hi there");
         for (int i = 1; i < argc; ++i)
